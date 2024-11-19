879891a10895f00000ad5c4206034ceb612b6d0c
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-11-19 11:20:33 +0900

    [Feat]: 큐브 위치 이동 성공

--- Q_01/Assets/Scripts/CubeController.cs
+++ Q_01/Assets/Scripts/CubeController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
-    public Vector3 SetPoint { get; private set; }
+    public Vector3 SetPoint { get;  set; } //private 제거 (private이면 다른 곳에서 쓸 수 없음)
 
     public void SetPosition()
     {
--- Q_01/Assets/Scripts/CubeManager.cs
+++ Q_01/Assets/Scripts/CubeManager.cs
@@ -28,13 +28,17 @@ public class CubeManager : MonoBehaviour
         _cubeSetPoint.x = x;
         _cubeSetPoint.y = y;
         _cubeSetPoint.z = z;
-        _cubeController.SetPosition();
+
+        _cubeController.SetPoint = _cubeSetPoint; //여기서 SetPoint에 _cubeSetPoint 값을 미리 설정 후 SetPosition을 호출해야 함
+        _cubeController.SetPosition(); //얘가 뭔지 아무 설정없이 바로 사용하고 있음 이렇게 되면 기본값으로 위치 변경
     }
 
     private void CreateCube()
     {
         GameObject cube = Instantiate(_cubePrefab);
         _cubeController = cube.GetComponent<CubeController>();
-        _cubeSetPoint = _cubeController.SetPoint;
+        //_cubeSetPoint = _cubeController.SetPoint;
+        //이 코드를 여기에 이렇게 작성하게 되면 Awake에서 저장된 (3,0,3)이 Start에서 CreateCube();가 호출될 때
+        //덮어씌워지기 때문에 (0,0,0)으로 위치를 이동하게 된다.
     }
 }
